8f03e2cc5ab577c59578f05c860f3faf18fa7a04
Mario Hayashi <mario@conversocial.com>
2016-02-18 11:45:06 +0000

    Create 'Don't merge my WIPs!' Chrome plugin v1.0

---
+++ README.md
@@ -0,0 +1,28 @@
+# Don't merge my WIPs!
+
+*Note:* Only works for [Github](https://github.com).
+
+This Chrome plugin warns you if you have WIPs (Work-In-Progress commits) in your pull request before merging.
+* Warns you when you view a pull request with WIPs.
+* Warns you and attempts to preempt JS execution when you try to merge a pull request with WIPs.
+
+Feel free to clone and modify however you like.
+
+## Screenshots
+
+![Chrome extension](/../master/screenshot-0.png?raw=true)
+
+![Warning #1](/../master/screenshot-1.png?raw=true)
+
+![Warning #2](/../master/screenshot-2.png?raw=true)
+
+## Installation
+1. Clone this repo
+2. Go to [chrome://extensions/](chrome://extensions/)
+3. Check 'Developer mode' checkbox
+4. Click 'Load unpacked extension...'
+5. Select the cloned repo from Step #1.
+6. Enable the plugin.
+
+## Credits
+Thanks to Gabe Berke-Williams who wrote this [great article on how to create a Chrome plugin.](https://robots.thoughtbot.com/how-to-make-a-chrome-extension)
---
+++ content.js
@@ -0,0 +1,33 @@
+var commitMessages = $('.commits-list-item .commit-title .message');
+var count = 0;
+$.each(commitMessages, function (key, message) {
+    var text = $(message).text();
+    if (new RegExp("wip", "gi").exec(text)) {
+        count += 1;
+    }
+});
+if (count > 0) {
+    alert('You have ' + count + ' WIPs to squash.');
+}
+var button = $('.js-pull-request-tab');
+if (button.length === 0) {
+    alert(
+        'The "Don\'t merge my WIPs!" Chrome plugin cannot find the '
+        + '"Merge pull request button". Please update the plugin.'
+    );
+}
+else {
+    $(button[0]).click(function(event) {
+        if (count === 0) {
+            alert('No WIPs found. Good job!');
+        }
+        if (count > 0) {
+            var errorMessage = 'What do you think you\'re doing? You have '
+                + count + ' WIPs to squash!';
+            alert(errorMessage);
+            event.preventDefault();
+            event.stopPropagation();
+            throw new Error(errorMessage);
+        }
+    });
+}
